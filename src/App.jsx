9a829520c0433f7509a3bb1ca5d71a220d555bb0
Khoatruong17 <truongndkgch190486@fpt.edu.vn>
2024-10-07 22:46:24 +0700

    update cors

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import axios from 'axios';
-
+import { useEffect, useState } from "react";
+import axios from "./util/axios.customize";
 
 function App() {
-  useEffect(()=>{
-    const fetchHelloWorld = async ()=>{
-      const res = await axios.get("localhost:8080/v1/api/")
-      console.log(">>>Check res: ",res)
-    }
-    fetchHelloWorld()
-  },[]);
-  return (
-    <>
-      Hello World
-    </>
-  )
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      const res = await axios.get(`/v1/api/`);
+      console.log(">>>Check res: ", res);
+    };
+    fetchHelloWorld();
+  }, []);
+  return <>Hello World</>;
 }
 
-export default App
+export default App;
---
+++ src/util/axios.customize.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+});
+
+instance.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    if (response && response.data) return response.data;
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
